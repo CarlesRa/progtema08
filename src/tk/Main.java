64727023a78a8b3d1ca8e794b67226e960f1f017
CarlesRa <jcramos8282@gmail.com>
2019-01-15 11:49:32 +0100

    añado el 3

--- src/tk/Main.java
+++ src/tk/Main.java
@@ -1,12 +1,11 @@
 package tk;
-import tk.carlesramos.progtema08.exercicistema08.Cercle;
-import tk.carlesramos.progtema08.exercicistema08.Punt;
+import tk.carlesramos.progtema08.exercici03.Principal03;
+import tk.carlesramos.progtema08.exercici04.Principal04;
 
 public class Main {
 
     public static void main(String[] args) {
-        Punt punt = new Punt();
-        punt.main();
-        Cercle cercle = new Cercle();
+        Principal03 exercici03 = new Principal03();
+        //Principal04 exercici04 = new Principal04();
     }
 }
